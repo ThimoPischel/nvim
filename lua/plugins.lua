331e9290f628a53b60312daacb08e4ce40a0971d
ThimoPischel <thimopischel@googlemail.com>
2022-04-20 20:55:52 +0200

    Porting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,9 +8,12 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'dhruvasagar/vim-table-mode'
   use 'junegunn/vim-easy-align'
+  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
 
   -- fzf -> http://neovimcraft.com/plugin/ibhagwan/fzf-lua/index.html
   use { 'junegunn/fzf', run = './install --bin', }
   use 'junegunn/fzf.vim'
 
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
 end)
